6569a0a4aba199a5cf875a019832004299f546f2
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-05 17:03:59 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.brown,
+          appBar: AppBar(
+            title: Text('I am poor'),
+            backgroundColor: Colors.cyanAccent,
+          ),
+          body: Center(
+              //child: Image(image: AssetImage()),
+              ),
+        ),
+      ),
+    );
